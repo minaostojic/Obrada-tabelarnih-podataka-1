a4ee7054df7784ecaebd4614666867a38262e43e
𝕸𝖎𝖓𝖆  𝕸 <>
2021-04-15 13:37:16 +0000

    Matrica

--- main.cs
+++ main.cs
@@ -2,6 +2,22 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
+    StreamReader podaci = new StreamReader("ulaz.txt");
+    string[,] matrica = new string[1000,6];
+    int brojac=0;
+    string s = podaci.ReadLine();
+    if (File.Exists("ulaz.txt"))
+    {
+      while (!podaci.EndOfStream)
+      {
+        s = podaci.ReadLine();
+        string[] elementi = s.Split(";"); //provera
+        for (int i=0; i<6; i++)
+        {
+          matrica[brojac,i] = elementi[i];
+        }
+        brojac++;
+      }
+    }
   }
 }
